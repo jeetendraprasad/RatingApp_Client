5f03168e67fef1574570e638c9320b95e768727e
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-06-25 16:52:17 +0530

    build script change

--- BuildScript.bat
+++ BuildScript.bat
@@ -1,6 +1,13 @@
 echo %~dp0
+
+SET PATH=%SystemRoot%\System32\WindowsPowerShell\v1.0\;%PATH%
+
+powershell -command " 'node version is' ; node --version; "
+node --version
+
 dir
 cd client
+powershell -command " npm install -g @angular/cli@15.0.0 "
 npm install -g @angular/cli@15.0.0
 ng version
 ng build
