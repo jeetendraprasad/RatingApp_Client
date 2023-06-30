b494c7a8bf47b51cb4998904381e24826b9588e0
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-06-30 09:35:54 +0530

    code changes

--- BuildScript.bat
+++ BuildScript.bat
@@ -1,5 +1,18 @@
-echo %~dp0
+@REM : BUILD SCRIPT AND REMOTE DEPLOYMENT
+@REM : THIS ASSUMES SERVER IP = %SERVER_IP%
+@REM : I HAVE DYNAMIC IP. SO I NEED TO CHANGE THE SERVER EVERY TIME IN THIS SCRIPT
 
+SET WORK_FOLDER=%~dp0
+SET WORK_FOLDER=%WORK_FOLDER:~0,-1%
+echo %WORK_FOLDER%
+
+@REM SERVER_IP IS DYNAMIC IP SO I HAVE TO CHANGE THAT ALWAYS.
+SET SERVER_IP=192.168.87.1
+SET SERVER_ADMINUSER=development
+@REM SERVER_FOLDER sould be present on server
+SET SERVER_FOLDER=c:\tmp
+
+SET PATH=%PATH%;%systemroot%\system32\inetsrv\;
 SET PATH=%SystemRoot%\System32\WindowsPowerShell\v1.0\;%PATH%
 
 powershell -command " 'node version is' ; node --version; "
@@ -21,11 +34,8 @@ powershell -command " npm.cmd i "
 
 powershell -command " ng.cmd build --base-href /eapp/ "
 
-dir
-tree
+scp -r %WORK_FOLDER%\bin\debug %SERVER_ADMINUSER%@%SERVER_IP%:%SERVER_FOLDER%\debug
 
-@REM PENDING is deployment in
-@REM local macine : C:\tmp\client
-@REM server : development@192.168.x.y
+ssh development@192.168.87.1 < ServerDeploymentCommands.txt
 
 exit /b 0
